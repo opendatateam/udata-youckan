fd65dd39910dbb6b5cb14812e781472876afc785
Axel Haustant <noirbizarre@gmail.com>
2017-01-10 13:47:06 +0100

    Update to version 0.9.1.dev for next development cycle

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ long_description = '\n'.join((
 
 setup(
     name='udata-youckan',
-    version='0.9.0',
+    version='0.9.1.dev',
     description='YouCKAN auth for uData',
     long_description=long_description,
     url='https://github.com/etalab/udata-youckan',
